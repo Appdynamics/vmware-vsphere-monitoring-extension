33569a53d6a03130a399c5720fda2b2e53983ba6
rvasanda <rohit.vasandani@osxltrvasa.local>
2013-10-11 15:54:37 -0700

    Bug fix for ACE 148
    - updated mailto link
    - support to display metrics for multiple vms

--- src/main/java/com/appdynamics/monitors/VMWare/VMWareMonitor.java
+++ src/main/java/com/appdynamics/monitors/VMWare/VMWareMonitor.java
@@ -1,11 +1,15 @@
 package com.appdynamics.monitors.vmware;
 
-import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
+import com.vmware.vim25.mo.*;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import com.singularity.ee.agent.systemagent.api.AManagedMonitor;
@@ -14,114 +18,129 @@ import com.singularity.ee.agent.systemagent.api.TaskExecutionContext;
 import com.singularity.ee.agent.systemagent.api.TaskOutput;
 import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
 import com.vmware.vim25.VirtualMachineQuickStats;
-import com.vmware.vim25.mo.*;
 
 public class VMWareMonitor extends AManagedMonitor
 {
 	/**
-	 * The metric can be found in Application Infrastructure Performance|{@literal <}Node{@literal >}|Custom Metrics|VMWare|Status
+	 * The metric can be found in Application Infrastructure Performance|{@literal <}Node{@literal >}|Custom Metrics|vmware|Status
 	 */
-	public static String metricPrefix = "Custom Metrics|VMWare|Status|";
+	public static String metricPrefix = "Custom Metrics|vmware|Status|VM Name|";
 
 	private ServiceInstance serviceInstance;
 	protected volatile String host;
-	protected volatile String machineId;
+	protected volatile List<String> vmnames;
 	protected volatile String username;
 	protected volatile String password;
+    private ServerConnection serverConnection = null;
 
 	protected final Logger logger = Logger.getLogger(this.getClass().getName());
 
-	/**
+    public VMWareMonitor() {
+        logger.setLevel(Level.INFO);
+    }
+
+    /**
 	 * Fetches statistics from VSphere and uploads them to the controller
 	 * @throws Exception
 	 */
 	public void populate() throws Exception
 	{
-		Folder root = serviceInstance.getRootFolder();
-		ManagedEntity managedEntity = new InventoryNavigator(root).searchManagedEntity("VirtualMachine", machineId);
+        Folder root = serviceInstance.getRootFolder();
+        List<ManagedEntity> managedEntities = new ArrayList<ManagedEntity>();
+        // Get all VMs
+        if (vmnames.size() == 1 && vmnames.get(0).equals("*")) {
+            managedEntities = Arrays.asList(new InventoryNavigator(root).searchManagedEntities("VirtualMachine"));
+        }
+        // Get specific VMs
+        else {
+            for (String vmname : vmnames) {
+                managedEntities.add(new InventoryNavigator(root).searchManagedEntity("VirtualMachine", vmname));
+            }
+        }
+        for (ManagedEntity managedEntity: managedEntities) {
+            if (managedEntity == null || !(managedEntity instanceof VirtualMachine))
+            {
+                throw new Exception("Could not find VM");
+            }
+            VirtualMachineQuickStats vmStats = ((VirtualMachine)managedEntity).getSummary().getQuickStats();
 
-		if (managedEntity == null || !(managedEntity instanceof VirtualMachine))
-		{
-			throw new Exception("Could not find VM");
-		}
-
-		VirtualMachineQuickStats vmStats = ((VirtualMachine)managedEntity).getSummary().getQuickStats();
-
-		printMetric("Ballooned Memory", vmStats.balloonedMemory,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Compressed Memory", vmStats.compressedMemory,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Overhead Memory Consumed", vmStats.consumedOverheadMemory,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Distributed CPU Entitlement", vmStats.distributedCpuEntitlement,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Distributed Memory Entitlement", vmStats.distributedMemoryEntitlement,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Guest Memory Usage", vmStats.guestMemoryUsage,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Host Memory Usage", vmStats.hostMemoryUsage,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Overall CPU Usage", vmStats.overallCpuUsage,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Overall CPU Demand", vmStats.overallCpuDemand,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Private Memory", vmStats.privateMemory,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Shared Memory", vmStats.sharedMemory,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Static CPU Entitlement", vmStats.staticCpuEntitlement,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Static Memory Entitlement", vmStats.staticMemoryEntitlement,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Swapped Memory", vmStats.swappedMemory,
-			MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, 
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-		);
-		printMetric("Up Time", vmStats.uptimeSeconds, 
-			MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
-			MetricWriter.METRIC_TIME_ROLLUP_TYPE_CURRENT, 
-			MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_INDIVIDUAL
-		);
+            printMetric(managedEntity.getName() + "|Ballooned Memory", vmStats.balloonedMemory,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Compressed Memory", vmStats.compressedMemory,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Overhead Memory Consumed", vmStats.consumedOverheadMemory,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Distributed CPU Entitlement", vmStats.distributedCpuEntitlement,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Distributed Memory Entitlement", vmStats.distributedMemoryEntitlement,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Guest Memory Usage", vmStats.guestMemoryUsage,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Host Memory Usage", vmStats.hostMemoryUsage,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Overall CPU Usage", vmStats.overallCpuUsage,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Overall CPU Demand", vmStats.overallCpuDemand,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Private Memory", vmStats.privateMemory,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Shared Memory", vmStats.sharedMemory,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Static CPU Entitlement", vmStats.staticCpuEntitlement,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Static Memory Entitlement", vmStats.staticMemoryEntitlement,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Swapped Memory", vmStats.swappedMemory,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+            );
+            printMetric(managedEntity.getName() + "|Up Time", vmStats.uptimeSeconds,
+                    MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
+                    MetricWriter.METRIC_TIME_ROLLUP_TYPE_CURRENT,
+                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_INDIVIDUAL
+            );
+        }
+        logger.info("Printed metrics successfully...");
 	}
 
 	/**
@@ -132,12 +151,13 @@ public class VMWareMonitor extends AManagedMonitor
 	public void connect() throws RemoteException, MalformedURLException
 	{
 		String url = "https://" + host + "/sdk";
-
-		if (serviceInstance == null)
-		{
-			serviceInstance = new ServiceInstance(new URL(url), username, password, true);
-		}
-
+            try {
+                serviceInstance = new ServiceInstance(new URL(url), username, password, true);
+                serverConnection = serviceInstance.getServerConnection();
+            }
+            catch (Exception e) {
+                logger.error("Exception", e);
+            }
 		logger.info("Connection to: " + url + " Successful");
 	}
 
@@ -150,8 +170,7 @@ public class VMWareMonitor extends AManagedMonitor
 		{
 			return;
 		}
-		serviceInstance.getServerConnection().logout();
-
+		serverConnection.logout();
 		logger.info("Connection closed");
 	}
 
@@ -161,22 +180,28 @@ public class VMWareMonitor extends AManagedMonitor
 	 */
 	public TaskOutput execute(Map<String, String> arg0, TaskExecutionContext arg1) throws TaskExecutionException
 	{
-		try 
+        try
 		{
 			host = arg0.get("host");
-			machineId = InetAddress.getLocalHost().getHostName();
-			username = arg0.get("username");
-			password = arg0.get("password");
+            username = arg0.get("username");
+            password = arg0.get("password");
 
-			connect();
-			populate();
+            String[] splittingNames = arg0.get("vmnames").split(",");
+            for (int i = 0; i < splittingNames.length; i++) {
+                splittingNames[i] = splittingNames[i].replaceAll("\\s","");
+            }
+            vmnames = Arrays.asList(splittingNames);
+
+            connect();
+            populate();
 			close();
 		}
 		catch (Exception e)
 		{
-			return new TaskOutput("Failed with error: " + e);
+			logger.error("Exception: ", e);
+            return new TaskOutput("Failed with error: " + e);
 		}
-
+        logger.info("Finished execution");
 		return new TaskOutput("Finished execution");
 	}
 
